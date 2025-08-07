2ec7e15e5770fa3623c129a5ee64dc452d8e53d2
hitha-shetty <hitharaghava@gmail.com>
2025-08-07 15:44:46 +0530

    minor additions

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,8 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
    <div className="text-3xl font-bold text-indigo-500">
-     Discord Clone
+     <UserButton afterSignOutUrl="/" />
    </div>
   );
 }
