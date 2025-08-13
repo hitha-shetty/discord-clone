cb8aee81818bd28adb1e768f36b4a2a4a14d157b
hitha-shetty <hitharaghava@gmail.com>
2025-08-14 04:32:02 +0530

    displaying the uploaded image

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,46 +1,57 @@
 "use client";
 
 import { UploadDropzone } from "@/lib/uploadthing";
-import "@uploadthing/react/styles.css"
+import "@uploadthing/react/styles.css";
+import { X } from "lucide-react";
+import Image from "next/image";
 
-interface FileUploadProps{
-    endPoint : "serverImage" | "messageFile"
-    value : string,
-    onChange : (url? : string)=>void,
+interface FileUploadProps {
+  endPoint: "serverImage" | "messageFile";
+  value: string;
+  onChange: (url?: string) => void;
 }
 
-const FileUpload = ({
-    endPoint,
-    value,
-    onChange
- } : FileUploadProps) => {
-  
-  console.log("FileUpload rendering with endpoint:", endPoint);
-  
+const FileUpload = ({ endPoint, value, onChange }: FileUploadProps) => {
+  const fileType = value?.split(".").pop();
+
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image fill src={value} alt="upload" className="rounded-full" />
+        <button
+          onClick={()=>onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="!p-2">
       <UploadDropzone
         endpoint={endPoint}
-        onClientUploadComplete={(res)=>{
-            console.log("Upload complete:", res);
-            onChange(res?.[0].url)
+        onClientUploadComplete={(res) => {
+          console.log("Upload complete:", res);
+          onChange(res?.[0].ufsUrl);
         }}
-        onUploadError={(error: Error)=>{
-            console.error("Upload error:", error);
+        onUploadError={(error: Error) => {
+          console.error("Upload error:", error);
         }}
         onUploadBegin={(fileName) => {
-            console.log("Upload started:", fileName);
+          console.log("Upload started:", fileName);
         }}
         onUploadProgress={(progress) => {
-            console.log("Upload progress:", progress);
+          console.log("Upload progress:", progress);
         }}
         config={{
-            mode: "auto"
+          mode: "auto",
         }}
       />
     </div>
   );
-
 };
 
 export default FileUpload;
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images : {
+    domains:[
+      "uploadthing.com",
+      "p1fjueu4tv.ufs.sh"
+    ]
+  }
 };
 
 export default nextConfig;
